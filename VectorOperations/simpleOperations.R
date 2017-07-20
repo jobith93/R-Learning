df9a7bf557964e0b38fb3fd1e3fcaa51bfe9e0fb
Jobith M Basheer <jobithmbasheer@gmail.com>
2017-07-20 11:53:07 +0530

    Vector Operations

---
+++ VectorOperations/simpleOperations.R
@@ -0,0 +1,21 @@
+#simple operations
+
+a <- c(1,2,3,4,5)
+# vector math
+a+1
+a/2
+a*2
+a-1
+
+b <- c(2,3,4,5,6)
+# vector-vector math
+a-b
+a+b
+a*b
+a/b
+
+# vector comparison
+a==c(1,2,3,4,5)
+a==c(2,4,3,1,6)
+a>=c(1,2,3,4,5)
+
